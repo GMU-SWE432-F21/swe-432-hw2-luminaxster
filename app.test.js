0588be88a4e3e96df3462f93810163aea63de853
David Gonzalez <luminaxster@gmail.com>
2021-09-23 15:56:36 -0400

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -4,6 +4,7 @@ const app = require("./app");
 
 describe("GET / ", () => {
   test("It should respond with an array of cities", async () => {
+    await new Promise((r) => setTimeout(r, 2000));
     const response = await request(app).get("/cities");
     expect(response.body).toEqual(["Fairfax", "Vienna", "Falls Church", "Arlington"]);
     expect(response.statusCode).toBe(200);
