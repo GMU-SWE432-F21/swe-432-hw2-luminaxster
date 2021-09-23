80a85ebd1e472091520efcd19b269e4a0e588a78
David Gonzalez <luminaxster@gmail.com>
2021-09-23 15:56:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const cities = ["Fairfax", "Vienna", "Falls Church", "Arlington"];
+let cities = {};
+const getZip = async ()=>{
+    cities = await fetch("https://swe432tomcat.herokuapp.com/zipLookup/zip=22033");
+};
+getZip();
+
+//const cities = ["Fairfax", "Vienna", "Falls Church", "Arlington"];
 
 const populations = [24019, 16489, 14128, 236842];
 
@@ -10,8 +16,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/cities', (req, res) => {
-  return res.json(cities)
+app.get('/cities/:cityname', (req, res) => {
+  return res.json(cities.filter)
 })
 
 app.get('/populations', (req, res) => {
